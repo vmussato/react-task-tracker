d1953d5624003833a3e318c5775a82e9360ea81b
Vinicius <vinicius.mussato@gmail.com>
2021-05-08 18:41:09 -0300

    creating a list component

---
+++ src/components/Tasks.js
@@ -0,0 +1,31 @@
+const tasks = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'Feb 5th at 2:30pm',
+    reminder: true
+  },
+  {
+    id: 2,
+    text: 'Meeting at school',
+    day: 'Feb 5th at 1:30pm',
+    reminder: true
+  },
+  {
+    id: 3,
+    text: 'Food Shopping',
+    day: 'Feb 5th at 3:30pm',
+    reminder: false
+  }
+
+]
+
+const Tasks = () => {
+  return (
+    <>
+      {tasks.map(task => <h3 key={task.id}>{task.text}</h3>)}
+    </>
+  )
+}
+
+export default Tasks
