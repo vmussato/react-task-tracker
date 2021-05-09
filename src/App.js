f751437029441d991eec42237589f064d7257440
Vinicius <vinicius.mussato@gmail.com>
2021-05-09 11:33:17 -0300

    Deleting tasks

--- src/App.js
+++ src/App.js
@@ -27,10 +27,15 @@ function App() {
   
   ])
 
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
