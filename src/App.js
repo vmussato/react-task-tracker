0a8d699260fcccafc16112b687164c7ec3adfdcd
Vinicius <vinicius.mussato@gmail.com>
2021-05-09 12:14:50 -0300

    no tasks state

--- src/App.js
+++ src/App.js
@@ -35,7 +35,10 @@ function App() {
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      {
+        tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} />
+        : 'No tasks to show'
+      }
     </div>
   );
 }
